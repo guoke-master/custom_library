f640a66c8c55969fd4efec606f01f1491b92e915
guoke-master <wzxbupt@bupt.edu.cn>
2021-10-26 22:03:58 +0800

    修改线性表顺序存储的C语言实现和测试：liner_list/seq_list/C/*，添加文件注释，函数注释

--- linear_list/seq_list/C/seq_list.c
+++ linear_list/seq_list/C/seq_list.c
@@ -1,3 +1,11 @@
+/*****************************************************************************************************************************
+ * File name:	seq_list.c
+ * Author:	guoke
+ * Date:	2021.10.26
+ * Description:	C语言线性表顺序实现的源文件，该文件包含了seq_list.h中所有API的定义。
+ * History:	1. 2021.10.26 创建并完成初始版本
+ ****************************************************************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,7 +21,15 @@ typedef struct SEQ_LIST_METADATA{
 
 
 
-/* 构造一个空的容量为capacity的顺序线性表，操作失败返回NULL */
+/*
+ * Function:		SeqList_Create()
+ * Description:		构造一个空的容量为capacity的顺序线性表
+ * Time complexity:	O(1)
+ * Input:	
+ *		capacity:	int类型，说明构造的顺序线性表的容量
+ * Output:	
+ * Return:			SEQ_LIST*类型，创建成功则返回顺序线性表地址，否则返回NULL
+ */
 SEQ_LIST* SeqList_Create(int capacity)
 {
 	SEQ_LIST_METADATA* internal_seq_list = NULL;
@@ -54,7 +70,15 @@ SEQ_LIST* SeqList_Create(int capacity)
 
 
 
-/* 销毁顺序线性表空间，操作成功返回0，否则返回其他值 */
+/*
+ * Function:		SeqList_Destroy()
+ * Description:		销毁顺序线性表空间
+ * Time complexity:	O(1)
+ * Input:	
+ *		seq_list:	SEQ_LIST*类型，顺序线性表地址
+ * Output:	
+ * Return:			int类型，操作成功返回0，否则返回其他值
+ */
 int SeqList_Destroy(SEQ_LIST* seq_list)
 {
 	// 将外部的地址转换为内部的地址
@@ -84,7 +108,15 @@ int SeqList_Destroy(SEQ_LIST* seq_list)
 
 
 
-/* 将顺序线性表置为空表，操作成功返回0，否则返回其他值 */
+/*
+ * Function:		SeqList_Clear()
+ * Description:		将顺序线性表置为空表
+ * Time complexity:	O(1)
+ * Input:	
+ *		seq_list:	SEQ_LIST*类型，顺序线性表地址
+ * Output:	
+ * Return:			int类型，操作成功返回0，否则返回其他值
+ */
 int SeqList_Clear(SEQ_LIST* seq_list)
 {
 	// 将外部的地址转换为内部的地址
@@ -105,7 +137,15 @@ int SeqList_Clear(SEQ_LIST* seq_list)
 
 
 
-/* 若顺序线性表为空表，则返回1，否则返回0，操作失败则返回负数 */
+/*
+ * Function:		SeqList_IsEmpty()
+ * Description:		判断顺序线性表是否为空表
+ * Time complexity:	O(1)
+ * Input:	
+ *		seq_list:	SEQ_LIST*类型，顺序线性表地址
+ * Output:	
+ * Return:			int类型，若顺序线性表为空表，则返回1，否则返回0，操作失败则返回负数
+ */
 int SeqList_IsEmpty(SEQ_LIST* seq_list)
 {
 	// 将外部的地址转换为内部的地址
@@ -124,7 +164,15 @@ int SeqList_IsEmpty(SEQ_LIST* seq_list)
 
 
 
-/* 返回顺序线性表中元素个数，操作失败则返回负数 */
+/*
+ * Function:		SeqList_GetLength()
+ * Description:		返回顺序线性表中元素个数
+ * Time complexity:	O(1)
+ * Input:	
+ *		seq_list:	SEQ_LIST*类型，顺序线性表地址
+ * Output:	
+ * Return:			int类型，操作成功返回顺序线性表长度，否则返回负数
+ */
 int SeqList_GetLength(SEQ_LIST* seq_list)
 {
 	// 将外部的地址转换为内部的地址
@@ -143,7 +191,15 @@ int SeqList_GetLength(SEQ_LIST* seq_list)
 
 
 
-/* 返回顺序线性表的容量，操作失败则返回负数 */
+/*
+ * Function:		SeqList_GetCapacity()
+ * Description:		返回顺序线性表的容量
+ * Time complexity:	O(1)
+ * Input:	
+ *		seq_list:	SEQ_LIST*类型，顺序线性表地址
+ * Output:	
+ * Return:			int类型，操作成功返回顺序线性表容量，否则返回负数
+ */
 int SeqList_GetCapacity(SEQ_LIST* seq_list)
 {
 	// 将外部的地址转换为内部的地址
@@ -162,7 +218,16 @@ int SeqList_GetCapacity(SEQ_LIST* seq_list)
 
 
 
-/* 返回顺序线性表第pos个数据元素的值，操作失败则返回NULL */
+/*
+ * Function:		SeqList_GetElem()
+ * Description:		返回顺序线性表第pos个数据元素的值
+ * Time complexity:	O(1)
+ * Input:	
+ *		seq_list:	SEQ_LIST*类型，顺序线性表地址
+ *		pos:		int类型，需要获取的数据元素的下标
+ * Output:	
+ * Return:			SEQ_LIST_ELEM类型，操作成功返回数据元素，否则返回NULL
+ */
 SEQ_LIST_ELEM SeqList_GetElem(SEQ_LIST* seq_list, int pos) 
 {
 	// 将外部的地址转换为内部的地址
@@ -188,7 +253,17 @@ SEQ_LIST_ELEM SeqList_GetElem(SEQ_LIST* seq_list, int pos)
 
 
 
-/* 返回顺序线性表中第1个与elem满足关系compare()的数据元素的下标。若这样的数据元素不存在，则返回-1 */
+/*
+ * Function:		SeqList_LocateElem()
+ * Description:		返回顺序线性表中第1个与数据元素elem满足关系compare()的数据元素的下标
+ * Time complexity:	如果compare()时间复杂度为m，则本函数时间复杂度为O(n*m)
+ * Input:	
+ *		seq_list:	SEQ_LIST*类型，顺序线性表地址
+ *		elem:		SEQ_LIST_ELEM类型，compare()函数中第一个参数
+ *		compare:	int(*)(SEQ_LIST_ELEM, SEQ_LIST_ELEM)类型，函数指针
+ * Output:	
+ * Return:			int类型，顺序线性表中第1个与数据元素elem满足关系compare()的数据元素的下标。操作失败则返回负数
+ */
 int SeqList_LocateElem(SEQ_LIST* seq_list, SEQ_LIST_ELEM elem, int (*compare)(SEQ_LIST_ELEM, SEQ_LIST_ELEM))
 {
 	// 将外部的地址转换为内部的地址
@@ -224,7 +299,16 @@ int SeqList_LocateElem(SEQ_LIST* seq_list, SEQ_LIST_ELEM elem, int (*compare)(SE
 
 
 
-/* 如果current_elem是顺序线性表的元素，且不是第一个，则返回它的前驱元素，否则操作失败，返回NULL */
+/*
+ * Function:		SeqList_PriorElem()
+ * Description:		如果current_elem是顺序线性表的元素，且不是第一个，则返回它的前驱元素
+ * Time complexity:	O(n)
+ * Input:	
+ *		seq_list:			SEQ_LIST*类型，顺序线性表地址
+ *		current_elem:		SEQ_LIST_ELEM类型，以该元素作为参考，获取其前驱元素
+ * Output:	
+ * Return:			SEQ_LIST_ELEM类型，current_elem的前驱元素，操作失败则返回NULL
+ */
 SEQ_LIST_ELEM SeqList_PriorELem(SEQ_LIST* seq_list, SEQ_LIST_ELEM current_elem)
 {
 	// 将外部的地址转换为内部的地址
@@ -255,7 +339,16 @@ SEQ_LIST_ELEM SeqList_PriorELem(SEQ_LIST* seq_list, SEQ_LIST_ELEM current_elem)
 
 
 
-/* 如果current_elem是顺序线性表的元素，且不是最后一个，则返回它的后继元素，否则操作失败，返回NULL */
+/*
+ * Function:		SeqList_NextElem()
+ * Description:		如果current_elem是顺序线性表的元素，且不是最后一个，则返回它的后继元素
+ * Time complexity:	O(n)
+ * Input:	
+ *		seq_list:			SEQ_LIST*类型，顺序线性表地址
+ *		current_elem:		SEQ_LIST_ELEM类型，以该元素作为参考，获取其后继元素
+ * Output:	
+ * Return:			SEQ_LIST_ELEM类型，current_elem的后继元素，操作失败则返回NULL
+ */
 SEQ_LIST_ELEM SeqList_NextElem(SEQ_LIST* seq_list, SEQ_LIST_ELEM current_elem)
 {
 	// 将外部的地址转换为内部的地址
@@ -286,7 +379,17 @@ SEQ_LIST_ELEM SeqList_NextElem(SEQ_LIST* seq_list, SEQ_LIST_ELEM current_elem)
 
 
 
-/* 在顺序线性表中第pos个位置之前插入新的数据元素elem，顺序线性表长度加1；成功则返回插入位置，失败则返回-1 */
+/*
+ * Function:		SeqList_Insert()
+ * Description:		在顺序线性表中第pos个位置之前插入新的数据元素elem，顺序线性表长度加1
+ * Time complexity:	O(n)
+ * Input:	
+ *		seq_list:	SEQ_LIST*类型，顺序线性表地址
+ *		pos:		int类型，将数据元素插入下标为pos的位置
+ *		elem:		SEQ_LIST_ELEM类型，插入顺序线性表的元素
+ * Output:	
+ * Return:			int类型，插入成功则返回插入位置下标，插入失败则返回负数
+ */
 int SeqList_Insert(SEQ_LIST* seq_list, int pos, SEQ_LIST_ELEM elem)
 {
 	// 将外部的地址转换为内部的地址
@@ -332,7 +435,16 @@ int SeqList_Insert(SEQ_LIST* seq_list, int pos, SEQ_LIST_ELEM elem)
 
 
 
-/* 删除顺序线性表中下标为pos的元素，并返回其值，顺序线性表长度-1；如果操作失败，返回NULL */
+/*
+ * Function:		SeqList_Delete()
+ * Description:		删除顺序线性表中下标为pos的元素，并返回其值，顺序线性表长度-1
+ * Time complexity:	O(n)
+ * Input:	
+ *		seq_list:	SEQ_LIST*类型，顺序线性表地址
+ *		pos:		int类型，将数据元素插入下标为pos的位置
+ * Output:	
+ * Return:			SEQ_LIST_ELEM类型，删除成功则返回删除的元素，插入失败则返回NULL
+ */
 SEQ_LIST_ELEM SeqList_Delete(SEQ_LIST* seq_list, int pos)
 {
 	// 将外部的地址转换为内部的地址
@@ -380,7 +492,16 @@ SEQ_LIST_ELEM SeqList_Delete(SEQ_LIST* seq_list, int pos)
 
 
 
-/* 依次对顺序线性表的每个元素调用visit()，一旦visit()失败，则操作失败。操作成功返回0，否则返回其他值 */
+/*
+ * Function:		SeqList_Traverse()
+ * Description:		依次对顺序线性表的每个元素调用visit()，一旦visit()失败，则操作失败
+ * Time complexity:	如果visit()时间复杂度为m，则本函数时间复杂度为O(n*M)
+ * Input:	
+ *		seq_list:	SEQ_LIST*类型，顺序线性表地址
+ *		visit:		int(*)(SEQ_LIST_ELEM)类型，函数指针，以数据元素为参数
+ * Output:	
+ * Return:			int类型，操作成功则返回0，否则返回其他值
+ */
 int SeqList_Traverse(SEQ_LIST* seq_list, int (*visit)(SEQ_LIST_ELEM))
 {
 	// 将外部的地址转换为内部的地址
